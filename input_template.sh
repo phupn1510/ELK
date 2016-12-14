ba13b1832cb94aaeeb2d57078b8d9574b7296932
phupn1510 <phu.phan@axonactive.vn>
2016-12-14 14:57:02 +0700

    run thu

--- input_template.sh
+++ input_template.sh
@@ -1,5 +1,5 @@
 !#/bin/bash
-curl -XPUT 'http://localhost:9200/_template/topbeat' -d@./index_template/topbeat.template.json
-curl -XPUT 'http://localhost:9200/_template/filebeat' -d@./index_template/filebeat.template.json
-curl -XPUT 'http://localhost:9200/_template/packetbeat' -d@./index_template/packetbeat.template.json
-curl -XPUT 'http://localhost:9200/_template/collectd' -d@./index_template/collectd.template.json
+/usr/bin/curl -XPUT 'http://localhost:9200/_template/topbeat' -d@./index_template/topbeat.template.json
+/usr/bin/curl -XPUT 'http://localhost:9200/_template/filebeat' -d@./index_template/filebeat.template.json
+/usr/bin/curl -XPUT 'http://localhost:9200/_template/packetbeat' -d@./index_template/packetbeat.template.json
+/usr/bin/curl -XPUT 'http://localhost:9200/_template/collectd' -d@./index_template/collectd.template.json
