7d32e5329c0253b05f01cbccd332be7376631ac4
phupn1510 <phu.phan@axonactive.vn>
2016-12-14 22:25:33 +0700

    First Released

--- pre_install.sh
+++ pre_install.sh
@@ -8,4 +8,5 @@ net=$(ifconfig eth0 | awk '/inet addr/{print substr($2,6)}')
 /bin/sed -i 's/localhost/'$net'/g' kibana/config/kibana.yml
 /bin/sed -i 's/localhost/'$net'/g' logstash/config/first-pipeline.conf
 /bin/sed -i 's/localhost/'$net'/g' logstash/config/second-pipeline.conf
+/bin/sed -i 's/localhost/'$net'/g' input_template.sh
 sudo sysctl -w vm.max_map_count=262144
