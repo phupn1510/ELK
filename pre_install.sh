b29685410867961cdea7d42683e6eb71e26db58f
root <phupn1510@gmail.com>
2017-02-25 13:40:02 +0700

    remove agent heartbeat

--- pre_install.sh
+++ pre_install.sh
@@ -4,11 +4,11 @@ chmod 766 -R /opt/elasticsearch/data
 chmod 766 -R logstash/tmp
 net=$(ifconfig eth1 | awk '/inet addr/{print substr($2,6)}')
 #sed -i 's/original/new/g' file.txt
-
+chomod -R 777 ./kibana/kibana
 /bin/sed -i 's/localhost/'$net'/g' nginx/default.conf
 /bin/sed -i 's/localhost/'$net'/g' kibana/config/kibana.yml
-/bin/sed -i 's/localhost/'$net'/g' logstash/config/first-pipeline.conf
-/bin/sed -i 's/localhost/'$net'/g' logstash/config/second-pipeline.conf
+#/bin/sed -i 's/localhost/'$net'/g' logstash/config/first-pipeline.conf
+#/bin/sed -i 's/localhost/'$net'/g' logstash/config/second-pipeline.conf
 /bin/sed -i 's/localhost/'$net'/g' input_template.sh
 echo 'vm.max_map_count=262144' >> /etc/sysctl.conf
 /sbin/sysctl -p
