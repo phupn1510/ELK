698af9b4f2e5d5ce261082c58ae4122b8f51470f
root <phupn1510@gmail.com>
2017-03-10 15:22:32 +0700

    new1

--- agents/install_agent_debian.sh
+++ agents/install_agent_debian.sh
@@ -12,15 +12,18 @@ sudo apt-get install libpcap0.8
 /usr/bin/curl -L -O https://artifacts.elastic.co/downloads/beats/packetbeat/packetbeat-5.2.1-amd64.deb
 /usr/bin/dpkg -i packetbeat-5.2.1-amd64.deb
 
-/bin/sed -i 's/localhost/'$IP_ELK'/g' filebeat.yml
-/bin/sed -i 's/localhost/'$IP_ELK'/g' metricbeat.yml
-/bin/sed -i 's/localhost/'$IP_ELK'/g' packetbeat.yml
-/bin/sed -i 's/localhost/'$IP_ELK'/g' heartbeat.yml
+/bin/sed -i 's/127.0.0.1/'$IP_ELK'/g' filebeat.yml
+/bin/sed -i 's/127.0.0.1/'$IP_ELK'/g' metricbeat.yml
+/bin/sed -i 's/127.0.0.1/'$IP_ELK'/g' packetbeat.yml
+/bin/sed -i 's/127.0.0.1/'$IP_ELK'/g' heartbeat.yml
 cp filebeat.yml /etc/filebeat/filebeat.yml
 cp metricbeat.yml /etc/metricbeat/metricbeat.yml
 cp packetbeat.yml /etc/packetbeat/packetbeat.yml
 cp heartbeat.yml /etc/heartbeat/heartbeat.yml
 cp logstash-forwarder.crt /etc/pki/tls/certs/
+cp ca.pem /etc/ca.pem
+ cp client.crt /etc/client.crt
+ cp client.key /etc/client.key
 /etc/init.d/filebeat restart
 /etc/init.d/metricbeat restart
 /etc/init.d/packetbeat restart
