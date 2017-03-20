4d534d2ba32ebc340f8d42cb53ec7420baf93309
root <phupn1510@gmail.com>
2017-03-20 15:45:25 +0700

    new

---
+++ agents/New folder/Untitled1.ps1
@@ -0,0 +1,6 @@
+﻿cd 'C:\Program Files\Winlogbeat'
+#PowerShell.exe -ExecutionPolicy UnRestricted -File .\install-service-winlogbeat.ps1
+scripts\import_dashboards.exe -es http://222.255.166.70:9200
+#.\metricbeat.exe
+ #.\metricbeat.exe -c metricbeat.yml -e -v -d "*"
+Invoke-WebRequest -Method Put -InFile winlogbeat.template.json -Uri http://222.255.166.70:9200/_template/winlogbeat?pretty
